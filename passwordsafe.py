e06abdf84208902794f9c36d66ee1a325c1a4275
Danibeba <danibeba05ingenieroinformatico@gmail.com>
2026-09-17 19:31:01 +0200

    Mejora del código algo más simplicado donde se verifica correctamente que la contraseña cumpla los requisitos de: poseer 12 caracteres, tener al menos una mayuscula, un numero y un simbolo

--- passwordsafe.py
+++ passwordsafe.py
@@ -6,83 +6,57 @@ intentos_maximos = 3
 
 while num_intentos < intentos_maximos:
 
-    if len(password) >= 12: #Validación de si la contraseña tiene 12 carácteres
+    if len(password) >= 12:
+        tiene_numero = False
+        tiene_mayuscula = False
+        tiene_simbolo = False
 
-        passwordvalida = True
+        for letter in password:
 
-        for letter in password: #for para comprobar el resto de condiciones mientras el número de carácteres es 12.
+            if letter.isdigit():
+                tiene_numero = True
 
-            if letter.isdigit(): #comprobación de si tiene un número
+            if letter.isupper():
+                tiene_mayuscula = True
 
-                passwordvalida = True
+            if letter in simbolos:
+                tiene_simbolo = True
 
-            else: 
-                passwordvalida = False
+        if tiene_numero == False:
+            passwordvalida = False
+            print("Compruebe si su contraseña cumple los requisitos de tener un numero")
 
-                print ("Compruebe si su contraseña cumple los requisitos de tener 12 caracteres")
+        elif tiene_mayuscula == False:
+            passwordvalida = False
+            print("Compruebe si su contraseña cumple los requisitos de una mayúscula")
 
-                num_intentos+=1 
+        elif tiene_simbolo == False:
+            passwordvalida = False
+            print("Compruebe si su contraseña cumple los requisitos de tener un símbolo")
 
-                if num_intentos < intentos_maximos:
+        else:
+            passwordvalida = True
 
-                    password = str(input("Password:"))
-                else: 
-                    print ("Has superado el número de intentos posibles. Vuelve a intentarlo más tarde")
-                    break
-
-
-            if letter.isupper(): #comprobación de si tiene una mayúscula
-
-                passwordvalida = True
-
-            else:
-                passwordvalida = False
-
-                print ("Compruebe si su contraseña cumple los requisitos de una mayúscula")
-
-                num_intentos+=1 
-
-                if num_intentos < intentos_maximos:
-
-                    password = str(input("Password:"))
-                else: 
-                    print ("Has superado el número de intentos posibles. Vuelve a intentarlo más tarde")
-                    break    
-                
-               
-            if any (letra in simbolos for letra in password): #comprobación de si tiene un simbolo
-
-                passwordvalida = True
-
-            else: 
-                passwordvalida = False
-
-                print ("Compruebe si su contraseña cumple los requisitos de 12 caracteres, una mayuscula y un símbolo")
-
-                num_intentos+=1 
-
-                if num_intentos < intentos_maximos:
-
-                    password = str(input("Password:"))
-                else: 
-                    print ("Has superado el número de intentos posibles. Vuelve a intentarlo más tarde")
-                    break
-
-
-        if passwordvalida == True: 
+        if passwordvalida == True:
             break
-    else:
-        passwordvalida = False 
-        if num_intentos == 3: 
-            print ("Has superado el numero de intentos máximos permitidos")
 
-        print ("Compruebe si su contraseña cumple los requisitos de 12 caracteres, una mayuscula y un símbolo")
-        num_intentos+=1 
+        num_intentos += 1
+
         if num_intentos < intentos_maximos:
             password = str(input("Password:"))
-        if num_intentos == 3: 
-            print ("Has superado el numero de intentos máximos permitidos")
+        else:
+            print("Has superado el número de intentos posibles. Vuelve a intentarlo más tarde")
 
+    else:
+        passwordvalida = False
 
-if passwordvalida == True:
-    print ("Password valida")
+        print("Compruebe si su contraseña cumple los requisitos de 12 caracteres")
+        num_intentos += 1
+
+        if num_intentos < intentos_maximos:
+            password = str(input("Password:"))
+        else:
+            print("Has superado el número de intentos posibles. Vuelve a intentarlo más tarde")
+
+if (passwordvalida):
+    print ("La contraseña introducida es válida")
