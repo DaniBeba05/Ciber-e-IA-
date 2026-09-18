df9d5e256e3b3d9b7edbd182d7586fae4d9c7254
Danibeba <danibeba05ingenieroinformatico@gmail.com>
2026-09-18 18:43:26 +0200

    Código modificado con la inclusion de varias funciones que simplifican el código

--- passwordsafe.py
+++ passwordsafe.py
@@ -1,62 +1,99 @@
+def tiene_longitud_maxima (contraseña, minimo = 12 ):
+    longitud_maxima = False
+    if len(contraseña) >= minimo: 
+        longitud_maxima = True
+
+    return longitud_maxima
+
+def tiene_una_mayuscula (contraseña):
+    tiene_mayuscula = False
+
+    if any(caracter.isupper() for caracter in contraseña):
+        tiene_mayuscula = True
+
+    return tiene_mayuscula
+
+def tiene_un_numero (contraseña):
+    tiene_numero = False
+    if any(caracter.isdigit() for caracter in contraseña):
+        tiene_numero = True
+
+    return tiene_numero
+
+
+def tiene_un_simbolo (contraseña):
+
+    tiene_simbolo = False
+    simbolos = '.,@#?!>;:-_'
+
+    if any(caracter in simbolos for caracter in contraseña):
+        tiene_simbolo = True
+
+    return tiene_simbolo
+
+
+def comprobar_num_intentos(num_intentos, intentos_maximos):
+
+    return num_intentos < intentos_maximos
+
+
+
+def sumar_num_intentos ():
+    global num_intentos
+    num_intentos = num_intentos +  1
+
+    return num_intentos
+
+def comprobar_password_valida ():
+    passwordvalida = False
+
+    if tiene_longitud_maxima(password) and tiene_una_mayuscula(password) and tiene_un_numero(password) and tiene_un_simbolo(password):
+        passwordvalida = True
+
+    return passwordvalida
+
 password = str(input("Password:"))
-passwordvalida = False
-simbolos = '.,@#?!>;:-_'
-num_intentos = 0
 intentos_maximos = 3
+num_intentos = 0
 
-while num_intentos < intentos_maximos:
+while comprobar_num_intentos(num_intentos,intentos_maximos):
 
-    if len(password) >= 12:
-        tiene_numero = False
-        tiene_mayuscula = False
-        tiene_simbolo = False
+    if tiene_longitud_maxima(password):
 
-        for letter in password:
+        if tiene_una_mayuscula(password) == False:
+            
+            print("Compruebe si su contraseña cumple los requisitos de tener una mayuscula")
 
-            if letter.isdigit():
-                tiene_numero = True
+        elif tiene_un_numero(password) == False:
+           
+            print("Compruebe si su contraseña cumple los requisitos de tener un número")
 
-            if letter.isupper():
-                tiene_mayuscula = True
-
-            if letter in simbolos:
-                tiene_simbolo = True
-
-        if tiene_numero == False:
-            passwordvalida = False
-            print("Compruebe si su contraseña cumple los requisitos de tener un numero")
-
-        elif tiene_mayuscula == False:
-            passwordvalida = False
-            print("Compruebe si su contraseña cumple los requisitos de una mayúscula")
-
-        elif tiene_simbolo == False:
-            passwordvalida = False
+        elif tiene_un_simbolo(password) == False:
+            
             print("Compruebe si su contraseña cumple los requisitos de tener un símbolo")
 
-        else:
-            passwordvalida = True
 
-        if passwordvalida == True:
+        if comprobar_password_valida():
             break
 
-        num_intentos += 1
 
-        if num_intentos < intentos_maximos:
+        sumar_num_intentos()
+
+        if comprobar_num_intentos(num_intentos, intentos_maximos):
             password = str(input("Password:"))
         else:
             print("Has superado el número de intentos posibles. Vuelve a intentarlo más tarde")
 
     else:
-        passwordvalida = False
+       
 
         print("Compruebe si su contraseña cumple los requisitos de 12 caracteres")
-        num_intentos += 1
+        sumar_num_intentos()
 
-        if num_intentos < intentos_maximos:
+        if comprobar_num_intentos(num_intentos, intentos_maximos):
             password = str(input("Password:"))
         else:
             print("Has superado el número de intentos posibles. Vuelve a intentarlo más tarde")
 
-if (passwordvalida):
+if (comprobar_password_valida()):
     print ("La contraseña introducida es válida")
