7a99c691528106ade12ac18b0bff16cb952d76f5
Danibeba <danibeba05ingenieroinformatico@gmail.com>
2026-09-19 18:21:37 +0200

    Nueva versión del código actualizada donde podemos apreciar que hemos importado en el código un archivo llamado module el cual simplifica mucho el código y nos permite tener un código más limpio y legible

--- passwordsafe.py
+++ passwordsafe.py
@@ -1,156 +1,47 @@
-def tiene_longitud_maxima (contraseña, minimo = 12 ):
-    longitud_maxima = False
-    if len(contraseña) >= minimo: 
-        longitud_maxima = True
+import module
 
-    return longitud_maxima
-
-
-
-def tiene_una_mayuscula (contraseña):
-    tiene_mayuscula = False
-
-    if any(caracter.isupper() for caracter in contraseña):
-        tiene_mayuscula = True
-
-    return tiene_mayuscula
-
-
-
-def tiene_un_numero (contraseña):
-    tiene_numero = False
-    if any(caracter.isdigit() for caracter in contraseña):
-        tiene_numero = True
-
-    return tiene_numero
-
-
-
-
-def tiene_un_simbolo (contraseña):
-
-    tiene_simbolo = False
-    simbolos = '.,@#?!>;:-_'
-
-    if any(caracter in simbolos for caracter in contraseña):
-        tiene_simbolo = True
-
-    return tiene_simbolo
-
-
-
-
-def comprobar_num_intentos(num_intentos, intentos_maximos):
-
-    return num_intentos < intentos_maximos
-
-
-
-def sumar_num_intentos ():
-    global num_intentos
-    num_intentos = num_intentos +  1
-
-    return num_intentos
-
-
-
-def comprobar_password_valida():
-    passwordvalida = False
-
-    if tiene_longitud_maxima(password) and tiene_una_mayuscula(password) and tiene_un_numero(password) and tiene_un_simbolo(password):
-        passwordvalida = True
-
-    return passwordvalida
-
-
-def mensajedepasswordvalida():
-         
-    return "La contraseña introducida es valida"
-
-
-
-
-def mensajedesuperaciondeintentos():
-
-    return "Has superado el número de intentos posibles. Vuelve a intentarlo más tarde"
-
-
-
-
-def mensajerequisitomayuscula():
-
-    return "Compruebe si su contraseña cumple los requisitos de tener al menos una mayúscula"
-
-
-
-def mensajerequisitonumero():
-
-    return "Compruebe si su contraseña cumple los requisitos de tener al menos un número"
-
-
-
-def mensajerequisitosimbolo():
-
-    return "Compruebe si su contraseña cumple los requisitos de tener al menos un simbolo"
-
-
-
-def mensajerequisitonumcaracteres():
-
-    return "Compruebe si su contraseña cumple los requisitos de tener al menos 12 carácteres"
-
-
-
-def introducirpassword():
-
-    global password
-
-    return str(input ("Password:"))
-
-
-
-password = introducirpassword()
 intentos_maximos = 3
 num_intentos = 0
 
-while comprobar_num_intentos(num_intentos,intentos_maximos):
-
-    if tiene_longitud_maxima(password):
-
-        if tiene_una_mayuscula(password) == False:
-            
-            print(mensajerequisitomayuscula())
-
-        elif tiene_un_numero(password) == False:
-           
-            print(mensajerequisitonumero())
-
-        elif tiene_un_simbolo(password) == False:
-            
-            print(mensajerequisitosimbolo())
 
 
-        if comprobar_password_valida():
+while module.comprobar_num_intentos(num_intentos,intentos_maximos):
+
+    password = module.introducirpassword()
+
+    if module.tiene_longitud_maxima(password):
+
+        if not module.tiene_una_mayuscula(password):
+
+            print(module.mensajerequisitomayuscula())
+
+        elif not module.tiene_un_numero(password):
+
+            print(module.mensajerequisitonumero())
+
+        elif not module.tiene_un_simbolo(password):
+
+            print(module.mensajerequisitosimbolo())
+
+
+        if module.comprobar_password_valida(password):
             break
 
 
-        sumar_num_intentos()
+        num_intentos = module.sumar_num_intentos(num_intentos)
 
-        if comprobar_num_intentos(num_intentos, intentos_maximos):
-            password = introducirpassword()
-        else:
-            print(mensajedesuperaciondeintentos())
+        if not module.comprobar_num_intentos(num_intentos,intentos_maximos):
+            print(module.mensajedesuperaciondeintentos())
 
     else:
-       
 
-        print("Compruebe si su contraseña cumple los requisitos de 12 caracteres")
-        sumar_num_intentos()
 
-        if comprobar_num_intentos(num_intentos, intentos_maximos):
-            password = introducirpassword()
-        else:
-            print(mensajedesuperaciondeintentos())
+        print(module.mensajerequisitonumcaracteres())
+        num_intentos = module.sumar_num_intentos(num_intentos)
 
-if (comprobar_password_valida()):
-    print (mensajedepasswordvalida())
+        if not module.comprobar_num_intentos(num_intentos,intentos_maximos):
+            print(module.mensajedesuperaciondeintentos())
+
+
+if (module.comprobar_password_valida(password)):
+    print (module.mensajedepasswordvalida())
